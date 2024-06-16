a35d694f1cdc69ba41f6b10537ba35d3550925ba
hesham-saeed <heshamsaid@hotmail.com>
2024-06-16 16:12:37 +0200

    add service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 	
+	private final StudentService studentService;
+	
+	@Autowired
+	public StudentController(StudentService studentService)
+	{
+		this.studentService = studentService;
+	}
+	
+	
 	@GetMapping() //serve as a RESTful endpoint
 	public List<Student> getStudents() {
-		return List.of(
-					new Student(
-							1L,
-							"Maryam",
-							24,
-							LocalDate.of(2000, Month.JANUARY, 5),
-							"Maryam@gmail.com")
-					
-				);
+		return studentService.getStudents();
 	}
 }
