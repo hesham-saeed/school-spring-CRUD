7a9d1e5f47d3522496e440cda791e7086ea71510
hesham-saeed <heshamsaid@hotmail.com>
2024-06-16 15:48:50 +0200

    add Student Controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,36 +1,16 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.student.Student;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@GetMapping() //serve as a RESTful endpoint
-	public List<Student> hello() {
-		return List.of(
-					new Student(
-							1L,
-							"Maryam",
-							24,
-							LocalDate.of(2000, Month.JANUARY, 5),
-							"Maryam@gmail.com")
-					
-				);
-		//return "Hello World";
-	}
+
 
 }
