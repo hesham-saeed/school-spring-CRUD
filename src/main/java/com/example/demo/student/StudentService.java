ae619e6f6a9dc4d6bb0f7c6f52715e498e871dc8
hesham-saeed <heshamsaid@hotmail.com>
2024-06-17 06:45:21 +0200

    add update functionality

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,12 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
@@ -37,6 +40,27 @@ public class StudentService {
 			throw new IllegalStateException("student with id " + id + " does not exists");
 		}
 		studentRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void updateStudent(Long id, String name, String email) {
+		Student student = studentRepository.findById(id)
+				.orElseThrow(() -> new IllegalStateException(
+						"student with id " + id + " does not exist"));
+		if (name != null & name.length() > 0 && !student.getName().equals(name))
+			student.setName(name);
+		
+		
+		if (email != null & email.length() > 0 && !student.getEmail().equals(email))
+		{
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if (studentOptional.isPresent())
+			{
+				throw new IllegalStateException("email taken");
+			}
+		}
+		student.setEmail(email);
+		
 	}
 	
 
